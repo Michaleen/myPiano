5304a7935fbbf5da3a42a38c38f75160a9056dc3
Michaleen <99398356+Michaleen@users.noreply.github.com>
2022-11-21 11:42:10 +0000

    basic html and layout of one octave on keyboard, changes in index.html, styles.css and with JS took control of keyboard div and put event listener on it.

--- piano.js
+++ piano.js
@@ -1,14 +1,25 @@
-//create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
+const keyBoard = document.getElementById('key-board');
 
+keyBoard.addEventListener('click', function(e){
+    console.log(e)
+})
+
+
+
+
+
+
+
+
+/*create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
 
 midC = document.getElementById('test-c-button');
 
 midC.addEventListener('click', function play(){
-    synth.triggerAttackRelease("C4", "8n");
-    
-    
+    synth.triggerAttackRelease("C4", "8n"); 
 })
+*/
 
 
 
