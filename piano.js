e4989b26942853f40b2b71783e390384d3beb4e3
Michaleen <99398356+Michaleen@users.noreply.github.com>
2022-11-21 23:18:56 +0000

    created Audio object and event listener and inserting html from piano.js. Added denseLead sounds patch folder in assets folder.

--- piano.js
+++ piano.js
@@ -1,17 +1,48 @@
 //creates a synth and connect it to the main output (speakers)
 const synth = new Tone.Synth().toDestination();
-
 const keyBoard = document.getElementById('key-board');
 
-keyBoard.addEventListener('click', function(e){
+// event listener for tone JS 
+/*keyBoard.addEventListener('click', function(e){
     console.log(e.target.id)
     synth.triggerAttackRelease(e.target.id, "8n")
+})*/
+
+//test one C4 wav file with audio object            //TOGGLE event listener for each patch or DRYER ??, layer sounds,
+keyBoard.addEventListener('click', function(e){             //  CHORUS (LONGER NOTES) MULTIPLE KEYS AT ONCE?
+    console.log(e.target.id)
+    new Audio(`assets/patches/denseLead/${e.target.id}.mp3`).play()
 })
+                                                            
+                        //   BUG NONE OF THE BLACK KEYS ARE PLAYING
+let octaveNum = 3;
+
+keyBoard.innerHTML = `<div class="whiteKey" id="C${octaveNum}">
+                    <div class="blackKey" id="C#${octaveNum}"></div>
+                    </div>
+                    <div class="whiteKey" id="D${octaveNum}">
+                    <div class="blackKey" id="D#${octaveNum}"></div>
+                    </div>
+                    <div class="whiteKey" id="E${octaveNum}">
+                    <div class="blackKey" id="blank1"></div>
+                    </div>
+                    <div class="whiteKey" id="F${octaveNum}">
+                    <div class="blackKey" id="F#${octaveNum}"></div>
+                    </div>
+                    <div class="whiteKey" id="G${octaveNum}">
+                    <div class="blackKey" ID="G#${octaveNum}"></div>
+                    </div>
+                    <div class="whiteKey" id="A${octaveNum}">
+                    <div class="blackKey" id="A#${octaveNum}"></div>
+                    </div>
+                    <div class="whiteKey" ID="B${octaveNum}">
+                    <div class="blackKey" id="blank2"></div>
+                    </div>`;
 
 
 
 
-
+//MOUSE click EVENT is clunky buggy with stuck sound
 
 
 
