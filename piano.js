e8f55ab877067357c42d336cd6fb9142c94ddeef
Michaleen <99398356+Michaleen@users.noreply.github.com>
2022-11-21 12:38:18 +0000

    passed keyboard.e.target.id to tone js synth in piano.js, added octave number to whie and black key id's

--- piano.js
+++ piano.js
@@ -1,7 +1,11 @@
+//creates a synth and connect it to the main output (speakers)
+const synth = new Tone.Synth().toDestination();
+
 const keyBoard = document.getElementById('key-board');
 
 keyBoard.addEventListener('click', function(e){
-    console.log(e)
+    console.log(e.target.id)
+    synth.triggerAttackRelease(e.target.id, "8n")
 })
 
 
@@ -11,15 +15,10 @@ keyBoard.addEventListener('click', function(e){
 
 
 
-/*create a synth and connect it to the main output (your speakers)
-const synth = new Tone.Synth().toDestination();
 
-midC = document.getElementById('test-c-button');
 
-midC.addEventListener('click', function play(){
-    synth.triggerAttackRelease("C4", "8n"); 
-})
-*/
+
+
 
 
 
