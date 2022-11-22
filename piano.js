77e8ea2b457ff0758d4b9631e6fb357e5583951c
Michaleen <99398356+Michaleen@users.noreply.github.com>
2022-11-22 11:31:30 +0000

    wrapped render keyboard in function in piano.js, added metronome assets, started mid controls panel index.html

--- piano.js
+++ piano.js
@@ -11,38 +11,54 @@ const keyBoard = document.getElementById('key-board');
 //test one C4 wav file with audio object            //TOGGLE event listener for each patch or DRYER ??, layer sounds,
 keyBoard.addEventListener('click', function(e){             //  CHORUS (LONGER NOTES) MULTIPLE KEYS AT ONCE?
     console.log(e.target.id)
+    console.log(e)
     new Audio(`assets/patches/denseLead/${e.target.id}.mp3`).play()
 })
-                                                            
-                        //   BUG NONE OF THE BLACK KEYS ARE PLAYING
-let octaveNum = 3;
+      
+//var ul = $('ul.rig');
+//var count = 15; // number of images
 
-keyBoard.innerHTML = `<div class="whiteKey" id="C${octaveNum}">
-                    <div class="blackKey" id="C#${octaveNum}"></div>
+//for(var i = 1; i <= count; i++) {
+ //   ul.append('<li><img src="images/' + i + '.jpg"></li>');
+//}
+
+
+let octaveNum = 6; //user input??
+let keyboardString ="";
+renderKeyBoard(octaveNum)
+
+function renderKeyBoard(octs) {                                                         // BUG NONE OF THE BLACK KEYS ARE PLAYING
+for (let i = 3; i <= octs; i++) {                   //nth child on divs to make this dryer??
+    keyboardString += (`<div class="whiteKey" id="C${i}">      
+                    <div class="blackKey" id="C#${i}"></div>
                     </div>
-                    <div class="whiteKey" id="D${octaveNum}">
-                    <div class="blackKey" id="D#${octaveNum}"></div>
+                    <div class="whiteKey" id="D${i}">
+                    <div class="blackKey" id="D#${i}"></div>
                     </div>
-                    <div class="whiteKey" id="E${octaveNum}">
+                    <div class="whiteKey" id="E${i}">
                     <div class="blackKey" id="blank1"></div>
                     </div>
-                    <div class="whiteKey" id="F${octaveNum}">
-                    <div class="blackKey" id="F#${octaveNum}"></div>
+                    <div class="whiteKey" id="F${i}">
+                    <div class="blackKey" id="F#${i}"></div>
                     </div>
-                    <div class="whiteKey" id="G${octaveNum}">
-                    <div class="blackKey" ID="G#${octaveNum}"></div>
+                    <div class="whiteKey" id="G${i}">
+                    <div class="blackKey" ID="G#${i}"></div>
                     </div>
-                    <div class="whiteKey" id="A${octaveNum}">
-                    <div class="blackKey" id="A#${octaveNum}"></div>
+                    <div class="whiteKey" id="A${i}">
+                    <div class="blackKey" id="A#${i}"></div>
                     </div>
-                    <div class="whiteKey" ID="B${octaveNum}">
+                    <div class="whiteKey" ID="B${i}">
                     <div class="blackKey" id="blank2"></div>
-                    </div>`;
+                    </div>`)
+                    console.log(i)
+                    console.log(keyboardString)
+                    console.log(octs)
+keyBoard.innerHTML = keyboardString;
+}};
 
+// metronome using timeouts and audio object
 
-
-
-//MOUSE click EVENT is clunky buggy with stuck sound
+//MOUSE click EVENT is clunky buggy with stuck sound options?
 
 
 
